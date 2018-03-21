f1b697c3d8f9948dd312b994be4fe4c61ef428fa
Julie <julie@MacBook-Pro.local>
2018-03-21 18:48:34 -0500

    updated name and password

--- environment.sh
+++ environment.sh
@@ -1,6 +1,6 @@
 export DB_HOST="tech-solutions.cz1g1wdkvjhg.us-east-2.rds.amazonaws.com"
-export DB_PASS=""
-export DB_USER=""
-export DB_PORT=""
+export DB_PASS="lancer98"
+export DB_USER="heckman"
+export DB_PORT="5432"
 export DB_NAME="deals"
 export PATH=$PATH:/usr/bin/google-chrome-stable
