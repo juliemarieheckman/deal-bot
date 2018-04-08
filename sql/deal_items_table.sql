c8469a152cd292b4314e4387370e0aaf296b71b1
Julie <julie@MacBook-Pro.local>
2018-04-08 18:54:48 -0500

    moved code to another repo for organization

--- sql/deal_items_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE deal_items (
-    deal_item_id SERIAL PRIMARY KEY,
-    source varchar,
-    start_dt date,
-    end_dt date,
-    value varchar,
-    item varchar,
-    details varchar
-);
